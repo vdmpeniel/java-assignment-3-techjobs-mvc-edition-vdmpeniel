d95e4e95e737fbe8825235f51199ce093ecc384b
victor diaz <vdmpeniel@yahoo.es>
2022-09-21 13:00:01 -0400

    All requirements but bonuses done.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
@@ -23,12 +23,14 @@ public class ListController {
     static HashMap<String, Object> tableChoices = new HashMap<>();
 
     public ListController () {
+
         columnChoices.put("all", "All");
         columnChoices.put("employer", "Employer");
         columnChoices.put("location", "Location");
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+        tableChoices.put("all", "View All");
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
@@ -37,8 +39,10 @@ public class ListController {
 
     @GetMapping(value = "")
     public String list(Model model) {
+
         model.addAttribute("columns", columnChoices);
         model.addAttribute("tableChoices", tableChoices);
+
         model.addAttribute("employers", JobData.getAllEmployers());
         model.addAttribute("locations", JobData.getAllLocations());
         model.addAttribute("positions", JobData.getAllPositionTypes());
--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,17 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Map;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -15,12 +23,32 @@ import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoic
 @RequestMapping("search")
 public class SearchController {
 
+
     @GetMapping(value = "")
     public String search(Model model) {
+        model.addAttribute("searchTerm", "");
+        model.addAttribute("searchType", "all");
         model.addAttribute("columns", columnChoices);
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam(name="searchType") String searchType, @RequestParam(name="searchTerm") String searchTerm){
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("columns", columnChoices);
+        searchTerm = searchTerm.toLowerCase();
 
+        ArrayList<Job> jobs;
+        if (searchTerm.isEmpty() || searchTerm.equals("all")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 }
